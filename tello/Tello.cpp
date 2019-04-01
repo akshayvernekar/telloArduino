7ab863666b58641b6f37c5816cec9cb70ccfed72
akshayvernekar <vernekarakshay@yahoo.com>
2019-04-01 15:23:42 +0800

    first commit

---
+++ tello/Tello.cpp
@@ -0,0 +1,363 @@
+/*
+ * Tello.cpp
+ *
+ *  Created on: Mar 26, 2019
+ *      Author: akki
+ */
+
+#include "Tello.h"
+
+static const char* TELLOIPADDRESS = "192.168.10.1";
+static const int COMMAND_PORT = 8889 ;
+static const int STATUS_PORT = 8890;
+#define MAXBUFFSIZE 512
+
+
+using namespace std;
+
+Tello::Tello()
+{
+	isInitialised = false;
+}
+
+Tello::~Tello()
+{
+
+}
+
+void Tello::init()
+{
+	udpclient = new WiFiUDP();
+	//This initializes udp and transfer buffer
+	udpclient->begin(COMMAND_PORT);
+
+	string response = sendCommand("command");
+	if(!response.compare("ok"))
+	{
+		isInitialised = true;
+	}
+}
+
+
+string Tello::sendCommand(string command)
+{
+	if(!isInitialised)
+		return "error";
+
+	udpclient->beginPacket(TELLOIPADDRESS, COMMAND_PORT);
+  	udpclient->write((const unsigned char*)command.c_str(), command.length());
+  	udpclient->endPacket();
+
+	char buffer[MAXBUFFSIZE]={0};
+	udpclient->parsePacket();
+	int n = udpclient->read(buffer, MAXBUFFSIZE);
+	if(n > 0)
+	{
+		buffer[n] = '\0';
+	}
+
+	return string(buffer);
+}
+
+bool Tello::takeoff()
+{
+	string response = sendCommand("takeoff");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::land()
+{
+	string response = sendCommand("land");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::startVideoStream()
+{
+	string response = sendCommand("streamon");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::stopVideoStream()
+{
+	string response = sendCommand("streamoff");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::turnOff()
+{
+	string response = sendCommand("emergency");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::up(int x)
+{
+	char buff[10];
+	string command = "up " + string(itoa(x,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::down(int x)
+{
+	char buff[10];
+	string command = "down " + string(itoa(x,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::left(int x)
+{
+	char buff[10];
+	string command = "left " + string(itoa(x,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::right(int x)
+{
+	char buff[10];
+	string command = "right " + string(itoa(x,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::forward(int x)
+{
+	char buff[10];
+	string command = "forward " + string(itoa(x,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::back(int x)
+{
+	char buff[10];
+	string command = "back " + string(itoa(x,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::rotate_clockwise(int deg)
+{
+	char buff[10];
+	string command = "cw " + string(itoa(deg,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::rotate_anticlockwise(int deg)
+{
+	char buff[10];
+	string command = "ccw " + string(itoa(deg,buff,10));
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::flip_front()
+{
+	string response = sendCommand("flip f");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::flip_right()
+{
+	string response = sendCommand("flip r");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::flip_left()
+{
+	string response = sendCommand("flipe l");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::flip_back()
+{
+	string response = sendCommand("flip b");
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Tello::setSpeed(int x)
+{
+	char buff[10];
+	string command = "speed " + string(itoa(x,buff,10));
+	string response ;
+	if((x < 10) || (x > 100))
+	{
+		//cout << "speed out of range " << endl;
+		return false;
+	}
+
+	response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+//TODO: need to understand this RC stuff
+bool Tello::sendRCcontrol(int x,int y,int z)
+{
+	return false;
+}
+
+bool Tello::changeWifi(string ssid, string password)
+{
+	string command = "wifi "+ ssid + " " +password;
+	string response = sendCommand(command);
+	if(!response.compare("ok"))
+	{
+		return true;
+	}
+	return false;
+}
+
+int Tello::getSpeed()
+{
+	string command = "speed?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+
+}
+
+int Tello::getBattery()
+{
+	string command = "battery?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getTime()
+{
+	string command = "time?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getHeight()
+{
+	string command = "height?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getTemperature()
+{
+	string command = "temp?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getAltitude()
+{
+	string command = "attitude?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getBaro()
+{
+	string command = "baro?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getAcceleration()
+{
+	string command = "acceleration?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getTof()
+{
+	string command = "tof?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
+
+int Tello::getWifiSnr()
+{
+	string command = "wifi?";
+	string response = sendCommand(command);
+	int x = atoi(response.c_str());
+	return x;
+}
---
+++ tello/Tello.h
@@ -0,0 +1,69 @@
+/*
+ * Tello.h
+ *
+ *  Created on: Mar 26, 2019
+ *      Author: akki
+ */
+
+#ifndef TELLO_H_
+#define TELLO_H_
+
+#include <string.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <string>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <stdio.h>
+
+using namespace std;
+class Tello
+{
+private :
+	bool isInitialised ;
+	WiFiUDP* udpclient;
+	string sendCommand(string command);
+
+public:
+	Tello();
+	~Tello();
+	
+	void init();
+
+	bool takeoff();
+	bool land();
+	bool startVideoStream();
+	bool stopVideoStream();
+	bool turnOff();
+	bool up(int x);
+	bool down(int x);
+	bool left(int x);
+	bool right(int x);
+	bool forward(int x);
+	bool back(int x);
+	bool rotate_clockwise(int deg);
+	bool rotate_anticlockwise(int deg);
+
+	bool flip_front();
+	bool flip_right();
+	bool flip_left();
+	bool flip_back();
+
+	bool setSpeed(int x);
+	bool sendRCcontrol(int x,int y,int z);
+	bool changeWifi(string ssid, string password);
+
+	int getSpeed();
+	int getBattery();
+	int getTime();
+	int getHeight();
+	int getTemperature();
+	int getAltitude();
+	int getBaro();
+	int getAcceleration();
+	int getTof();
+	int getWifiSnr();
+
+};
+
+#endif /* TELLO_H_ */
